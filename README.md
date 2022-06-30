2b8f973c73f35ee017f8f2c1f13ac0e48583fbbd
Sushrit Pasupuleti <sushrit.pk21@gmail.com>
2022-06-30 12:04:22 +0530

    feat: add morgan

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # Fullstack Logging
- An End-to-End Logging Solution for PERN Stack (Next.js)
+ 
+An End-to-End Logging Solution for PERN Stack (Next.js)
+
+## Express.js
+
+### Morgan
+
+Refer to [docs](https://github.com/expressjs/morgan)
+
+```node
+app.use(
+	morgan('<format>')
+)
+```
--- server/index.js
+++ server/index.js
@@ -2,15 +2,29 @@ const express = require('express')
 const app = express()
 const port = 5000
 const cors = require('cors');
+var fs = require('fs')
+var morgan = require('morgan')
+var path = require('path')
 
 app.use(cors({
     origin: 'http:/localhost:3000'
 }));
+
+// create a write stream (in append mode)
+var accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' })
+
+
+// setup the logger
+app.use(morgan('combined', { stream: accessLogStream }))
+// app.use(
+// 	morgan(':method :url :status :res[content-length] - :response-time ms', {stream: accessLogStream})
+// )
 
 app.get('/', (req, res) => {
   res.send('Hello There.')
 })
 
 app.listen(port, () => {
-  console.log(`Server listening on http://localhost:${port} 🚀`)
+	console.log(`Server listening on http://localhost:${port} 🚀`)
+	console.log("Writing Logs to: ", path.join(__dirname, 'access.log'))
 })
